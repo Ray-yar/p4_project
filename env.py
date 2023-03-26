2e4b85afd4b857f0375821dbefa42b466fa1837a
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-03-26 18:08:18 +0000

    new changes

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://yiofuavn:SrQo4D96kDHcbzA42UWCwPysQU_752_1@kandula.db.elephantsql.com/yiofuavn"
 os.environ["SECRET_KEY"] = "Father"
+os.environ["CLOUDINARY_URL"] = "cloudinary://948949725979254:t24F3U0RxeG0sSSR5SeYeBN1k3U@dijdx1fv1"
